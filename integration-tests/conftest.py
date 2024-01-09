8129ce4dadbe2ed666c9d845af33b4f43a8a136f
root <root@dell-per740-68-vm-03.lab.eng.pek2.redhat.com>
2024-01-09 05:35:26 -0500

    add test folder integration-tests and define fixture to install katello rpm

---
+++ integration-tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+import logging
+import os
+import subprocess
+
+@pytest.fixture(scope="session", autouse=True)
+def install_katello_rpm():
+    the_environment = os.getenv('ENV_FOR_DYNACONF')
+    logging.info("Env for dynaconf: %s" % the_environment)
+    if "satellite" in the_environment:
+        logging.info("Start to configure the rhel for satellite")
+        #satellite hostname will be gotton from dynaconf or CI job parameter, not decide the final solution
+        satellite_hostname = "ci-vm-10-0-96-150.hosted.upshift.rdu2.redhat.com"
+        cmd = 'rpm -qa | grep kate | xargs rpm -e;rpm -Uvh http://%s/pub/katello-ca-consumer-%s-1.0-1.noarch.rpm' % (satellite_hostname, satellite_hostname)
+        ret, _ = subprocess.getstatusoutput(cmd)
+        assert ret == 0
